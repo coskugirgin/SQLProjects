dc8f290ceb1c52aa45e061cad1071a0700b8ef88
coskugirgin <coskugirgin@gmail.com>
2023-05-21 01:41:36 +0300

    SQLProject6 fixed

--- SQLProject6.sql
+++ SQLProject6.sql
@@ -14,7 +14,7 @@ WHERE title LIKE 'C%';
 SELECT MAX(length) FROM film
 WHERE rental_rate = 0.99;
 
-3. 'How many different replacement_cost values are there for the movies in the film table that are longer than 150 minutes?'
+4. 'How many different replacement_cost values are there for the movies in the film table that are longer than 150 minutes?'
 
 SELECT COUNT (DISTINCT replacement_cost) FROM film
 WHERE length > 150;
