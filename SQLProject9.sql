cbcb3ee8f56e4520679607e57284b75077747480
coskugirgin <coskugirgin@gmail.com>
2023-05-26 00:28:42 +0300

    SQLProject9 fixed

--- SQLProject9.sql
+++ SQLProject9.sql
@@ -3,8 +3,8 @@
 1. 'Write the INNER JOIN query where we can see the city and country names in the city table
  and the country table together.'
 
-SELECT city, country FROM country
-INNER JOIN city ON city.country_id = country.country_id;
+SELECT city, country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
 
 2. 'Write the INNER JOIN query where we can see the payment_id, from the payment table, 
 the first_name and last_name, from the customer table together.'
