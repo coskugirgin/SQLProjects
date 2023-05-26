3320a42ddf06f68dc708e9b561085beb04d5a697
coskugirgin <coskugirgin@gmail.com>
2023-05-26 03:17:52 +0300

    SQLProject12 fixed

--- SQLProject12.sql
+++ SQLProject12.sql
@@ -18,6 +18,6 @@ WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (
 4. 'In the payment table, list the customers who make the most purchases.'
 
 SELECT customer.first_name, customer.last_name, COUNT(rental_id) FROM payment
-RIGHT JOIN customer ON customer.customer_id = payment.customer_id
+JOIN customer ON customer.customer_id = payment.customer_id
 GROUP BY customer.customer_id, customer.first_name, customer.last_name, payment.customer_id
 ORDER BY COUNT(rental_id) DESC;
